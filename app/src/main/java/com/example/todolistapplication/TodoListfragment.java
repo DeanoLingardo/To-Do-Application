30d95d4adf22b11115fa3e4d282c8295215a118b
Dean <deanlingard@yahoo.co.uk>
2017-11-17 14:47:58 +0000

    Toolbar complete

--- app/src/main/java/com/example/todolistapplication/TodoListfragment.java
+++ app/src/main/java/com/example/todolistapplication/TodoListfragment.java
@@ -11,6 +11,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -28,7 +31,13 @@ public class TodoListfragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_todo_list, menu);
     }
 
     @Override
@@ -110,6 +119,24 @@ public class TodoListfragment extends Fragment {
             mTextViewDate.setText(mTodo.getDate().toString());
         }
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.new_todo:
+
+                Todo todo = new Todo();
+                TodoModel.get(getActivity()).addTodo(todo);
+
+                Intent intent = TodoActivity.newIntent(getActivity(), todo.getId());
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public class TodoAdapter extends RecyclerView.Adapter<TodoListfragment.TodoHolder> {
